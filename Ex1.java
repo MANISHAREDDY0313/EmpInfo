e81f9aedb4020ac1527c70f2df2dd95de8bdc18f
MANISHAREDDY0313 <77886776+MANISHAREDDY0313@users.noreply.github.com>
2021-01-26 14:55:47 +0530

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -3,7 +3,7 @@ package basic.basics;
 public class Ex1 {
 	//main method
  public static void main(String[] args) {
-	// write the code or logic
+	// write the code or logic here
 	 
 	 System.out.println("Hello..........");
 }
