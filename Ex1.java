e652913c6b42fa4d2737127e717e0a5e8f9c7329
MANISHAREDDY0313 <77886776+MANISHAREDDY0313@users.noreply.github.com>
2021-01-26 14:43:18 +0530

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,10 @@
+package basic.basics;
+
+public class Ex1 {
+	//main method
+ public static void main(String[] args) {
+	// write the code or logic
+	 
+	 System.out.println("Hello..........");
+}
+}
---
+++ Ex2.java
@@ -0,0 +1,8 @@
+package basic.basics;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to java program");
+	}
+}
